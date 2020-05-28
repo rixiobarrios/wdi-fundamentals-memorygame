a99d9fd5fe29de137192d2756217713f9062bffe
Rixio Barrios <55994508+rixiobarrios@users.noreply.github.com>
2020-05-28 15:05:40 -0500

    First Commit/Files uploaded

---
+++ README.md
@@ -0,0 +1,6 @@
+# wdi-fundamentals-memorygame
+This is a repository that contains a game as part of my bootcamp lessons
+# wdi-fundamentals-memorygame
+# wdi-fundamentals-memorygame
+# sei129-wdi-fundamentals-memorygame
+# sei129-wdi-fundamentals-memorygame
---
+++ js/main.js
@@ -0,0 +1,90 @@
+console.log('Up and running!');
+
+var cards = [
+    {
+        rank: 'queen',
+        suit: 'hearts',
+        cardImage: 'images/queen-of-hearts.png'
+    },
+    {
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: 'images/queen-of-diamonds.png'
+    },
+    {
+        rank: 'king',
+        suit: 'hearts',
+        cardImage: 'images/king-of-hearts.png'
+    },
+    {
+        rank: 'king',
+        suit: 'diamonds',
+        cardImage: 'images/king-of-diamonds.png'
+    }
+];
+
+var cardsInPlay = [];
+var matchCounter = 0;
+
+var reset = function() {
+    for (var i = 0; i < cards.length; i++) {
+        document
+            .getElementsByTagName('img')
+            [i].setAttribute('src', 'images/back.png');
+    }
+    cardsInPlay = [];
+    matchCounter = 0;
+};
+
+function checkforMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log('You found a match!');
+    } else {
+        console.log('Sorry, try again.');
+    }
+}
+
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+    cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert('You found a match!');
+        } else if (cardsInPlay[0] !== cardsInPlay[1]) {
+            alert('Sorry, try again.');
+        }
+        return flipCard;
+    }
+
+    console.log('User flipped ' + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+}
+
+checkforMatch();
+
+var reset = function() {
+    for (var i = 0; i < cards.length; i++) {
+        document
+            .getElementsByTagName('img')
+            [i].setAttribute('src', 'images/back.png');
+    }
+};
+
+function createBoard() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+function pageReset() {
+    document.getElementById('reset-button');
+}
+
+createBoard();
+pageReset();
